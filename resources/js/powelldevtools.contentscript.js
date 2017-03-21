4437e824cf01bc08740f6abcec3d2d01c4c0fc50
Serge ARADJ <serge.aradj@gmail.com>
2017-03-21 10:11:42 +0100

    fix error in content script

--- resources/js/powelldevtools.contentscript.js
+++ resources/js/powelldevtools.contentscript.js
@@ -9,25 +9,28 @@
         });
 
     var actualCode = '(' + function(angular) {
-        angular.module('app')
-            .config(["$provide", function($provide) {
-                $provide.decorator("$templateCache", ["$delegate", function($delegate) {
-                    console.log('OK Delegate');
-                    // Stash away the original get method
-                    var origPutMethod = $delegate.put;
-                    $delegate.templatesArray = []
-                        // Replace it with our getter
-                    $delegate.put = function(url, content) {
-                        // Do we match our content type family prefix?
-                        $delegate.templatesArray.push(url);
-                        console.log('$templateCache:', $delegate.templatesArray);
-                        // Otherwise, use the original get method
-                        return origPutMethod(url, content);
-                    };
+        if (angular && angular.module) {
+            angular.module('app')
+                .config(["$provide", function($provide) {
+                    debugger;
+                    $provide.decorator("$templateCache", ["$delegate", function($delegate) {
+                        console.log('OK Delegate');
+                        // Stash away the original get method
+                        var origPutMethod = $delegate.put;
+                        $delegate.templatesArray = []
+                            // Replace it with our getter
+                        $delegate.put = function(url, content) {
+                            // Do we match our content type family prefix?
+                            $delegate.templatesArray.push(url);
+                            console.log('$templateCache:', $delegate.templatesArray);
+                            // Otherwise, use the original get method
+                            return origPutMethod(url, content);
+                        };
 
-                    return $delegate;
+                        return $delegate;
+                    }]);
                 }]);
-            }]);
+        }
     } + ')(window.angular);';
     var script = document.createElement('script');
     script.textContent = actualCode;
