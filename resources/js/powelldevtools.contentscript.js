78a54b9986aa60cf2415a178d0c131d280d35c58
Serge ARADJ <serge.aradj@gmail.com>
2017-03-21 10:11:27 +0100

    fix error in content script

--- resources/js/powelldevtools.contentscript.js
+++ resources/js/powelldevtools.contentscript.js
@@ -1,47 +1,38 @@
 (function(chrome, localStorage, sessionStorage, document) {
-    var deepClearCache = function(cacheHandler) {
-        cacheHandler._keys.forEach(function(key) {
-            delete key;
-        });
-    }
-
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
-            if (request.method == "powDevTools.clearStorage")
+            if (request.method == "powDevTools.clearStorage") {
                 localStorage.clear();
-            if (localStorage._keys && localStorage._keys.length > 0) {
-                deepClearCache(localStorage);
+                sessionStorage.clear();
+                sendResponse(true);
             }
-            sessionStorage.clear();
-            if (sessionStorage._keys && sessionStorage._keys.length > 0) {
-                deepClearCache(sessionStorage);
-            }
-            sendResponse(true);
         });
 
     var actualCode = '(' + function(angular) {
-        angular.module('app')
-            .config(["$provide", function($provide) {
-                $provide.decorator("$templateCache", ["$delegate", function($delegate) {
-                    console.log('OK Delegate');
-                    // Stash away the original get method
-                    var origPutMethod = $delegate.put;
-                    $delegate.templatesArray = []
-                        // Replace it with our getter
-                    $delegate.put = function(url, content) {
-                        // Do we match our content type family prefix?
-                        $delegate.templatesArray.push(url);
-                        console.log('$templateCache:', $delegate.templatesArray);
-                        // Otherwise, use the original get method
-                        return origPutMethod(url, content);
-                    };
+        if (angular && angular.module) {
+            angular.module('app')
+                .config(["$provide", function($provide) {
+                    debugger;
+                    $provide.decorator("$templateCache", ["$delegate", function($delegate) {
+                        console.log('OK Delegate');
+                        // Stash away the original get method
+                        var origPutMethod = $delegate.put;
+                        $delegate.templatesArray = []
+                            // Replace it with our getter
+                        $delegate.put = function(url, content) {
+                            // Do we match our content type family prefix?
+                            $delegate.templatesArray.push(url);
+                            console.log('$templateCache:', $delegate.templatesArray);
+                            // Otherwise, use the original get method
+                            return origPutMethod(url, content);
+                        };
 
-                    return $delegate;
+                        return $delegate;
+                    }]);
                 }]);
-            }]);
+        }
     } + ')(window.angular);';
     var script = document.createElement('script');
     script.textContent = actualCode;
     (document.head || document.documentElement).appendChild(script);
-    script.remove();
 })(window.chrome, window.localStorage, window.sessionStorage, window.document);
