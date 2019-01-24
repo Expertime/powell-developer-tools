cf3302052828be7b346923491df01d7d403afcfd
Serge ARADJ <serge.aradj@gmail.com>
2019-01-24 10:35:21 +0100

    Added lodash _.mergeWith

--- resources/js/lodash/lodash.custom.js
+++ resources/js/lodash/lodash.custom.js
@@ -1,7 +1,7 @@
 /**
  * @license
  * Lodash (Custom Build) <https://lodash.com/>
- * Build: `lodash include="get,set,intersection,cloneDeep,findIndex,merge"`
+ * Build: `lodash include="get,set,intersection,cloneDeep,findIndex,merge,mergeWith"`
  * Copyright JS Foundation and other contributors <https://js.foundation/>
  * Released under MIT license <https://lodash.com/license>
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
@@ -4111,6 +4111,41 @@
   });
 
   /**
+   * This method is like `_.merge` except that it accepts `customizer` which
+   * is invoked to produce the merged values of the destination and source
+   * properties. If `customizer` returns `undefined`, merging is handled by the
+   * method instead. The `customizer` is invoked with six arguments:
+   * (objValue, srcValue, key, object, source, stack).
+   *
+   * **Note:** This method mutates `object`.
+   *
+   * @static
+   * @memberOf _
+   * @since 4.0.0
+   * @category Object
+   * @param {Object} object The destination object.
+   * @param {...Object} sources The source objects.
+   * @param {Function} customizer The function to customize assigned values.
+   * @returns {Object} Returns `object`.
+   * @example
+   *
+   * function customizer(objValue, srcValue) {
+   *   if (_.isArray(objValue)) {
+   *     return objValue.concat(srcValue);
+   *   }
+   * }
+   *
+   * var object = { 'a': [1], 'b': [2] };
+   * var other = { 'a': [3], 'b': [4] };
+   *
+   * _.mergeWith(object, other, customizer);
+   * // => { 'a': [1, 3], 'b': [2, 4] }
+   */
+  var mergeWith = createAssigner(function(object, source, srcIndex, customizer) {
+    baseMerge(object, source, srcIndex, customizer);
+  });
+
+  /**
    * Sets the value at `path` of `object`. If a portion of `path` doesn't exist,
    * it's created. Arrays are created for missing index properties while objects
    * are created for all other missing properties. Use `_.setWith` to customize
@@ -4310,6 +4345,7 @@
   lodash.keysIn = keysIn;
   lodash.memoize = memoize;
   lodash.merge = merge;
+  lodash.mergeWith = mergeWith;
   lodash.property = property;
   lodash.set = set;
   lodash.toPlainObject = toPlainObject;
