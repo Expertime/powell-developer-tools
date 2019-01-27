756adf5d98015dc52f2c12dad31e391e413f5926
Serge ARADJ <serge.aradj@gmail.com>
2019-01-27 18:11:21 +0100

    Replaced lodash _.uniqWIth by _.uniqBy

--- resources/js/lodash/lodash.custom.js
+++ resources/js/lodash/lodash.custom.js
@@ -1,7 +1,7 @@
 /**
  * @license
  * Lodash (Custom Build) <https://lodash.com/>
- * Build: `lodash include="get,set,intersection,cloneDeep,findIndex,merge,uniqWith"`
+ * Build: `lodash include="get,set,intersection,cloneDeep,findIndex,merge,uniqBy"`
  * Copyright JS Foundation and other contributors <https://js.foundation/>
  * Released under MIT license <https://lodash.com/license>
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
@@ -3372,28 +3372,30 @@
   });
 
   /**
-   * This method is like `_.uniq` except that it accepts `comparator` which
-   * is invoked to compare elements of `array`. The order of result values is
-   * determined by the order they occur in the array.The comparator is invoked
-   * with two arguments: (arrVal, othVal).
+   * This method is like `_.uniq` except that it accepts `iteratee` which is
+   * invoked for each element in `array` to generate the criterion by which
+   * uniqueness is computed. The order of result values is determined by the
+   * order they occur in the array. The iteratee is invoked with one argument:
+   * (value).
    *
    * @static
    * @memberOf _
    * @since 4.0.0
    * @category Array
    * @param {Array} array The array to inspect.
-   * @param {Function} [comparator] The comparator invoked per element.
+   * @param {Function} [iteratee=_.identity] The iteratee invoked per element.
    * @returns {Array} Returns the new duplicate free array.
    * @example
    *
-   * var objects = [{ 'x': 1, 'y': 2 }, { 'x': 2, 'y': 1 }, { 'x': 1, 'y': 2 }];
+   * _.uniqBy([2.1, 1.2, 2.3], Math.floor);
+   * // => [2.1, 1.2]
    *
-   * _.uniqWith(objects, _.isEqual);
-   * // => [{ 'x': 1, 'y': 2 }, { 'x': 2, 'y': 1 }]
+   * // The `_.property` iteratee shorthand.
+   * _.uniqBy([{ 'x': 1 }, { 'x': 2 }, { 'x': 1 }], 'x');
+   * // => [{ 'x': 1 }, { 'x': 2 }]
    */
-  function uniqWith(array, comparator) {
-    comparator = typeof comparator == 'function' ? comparator : undefined;
-    return (array && array.length) ? baseUniq(array, undefined, comparator) : [];
+  function uniqBy(array, iteratee) {
+    return (array && array.length) ? baseUniq(array, getIteratee(iteratee, 2)) : [];
   }
 
   /*------------------------------------------------------------------------*/
@@ -4426,7 +4428,7 @@
   lodash.property = property;
   lodash.set = set;
   lodash.toPlainObject = toPlainObject;
-  lodash.uniqWith = uniqWith;
+  lodash.uniqBy = uniqBy;
 
   /*------------------------------------------------------------------------*/
 
