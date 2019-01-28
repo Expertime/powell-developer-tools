663f8fedbea05f4d5a6f0a7dded121fa7c0a9298
Serge ARADJ <serge.aradj@gmail.com>
2019-01-28 16:51:05 +0100

    Fix content_security_policy - updated to vers. 6.1.0

--- resources/js/popup.js
+++ resources/js/popup.js
@@ -23,7 +23,7 @@
         /**************
          * App version
          **************/
-        $scope.appVers = "6.0.0";
+        $scope.appVers = "6.1.0";
 
         /*****************
          * View variables
