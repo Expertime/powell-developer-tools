1d83aa72a36f42741f3eae4ae4f7e3ba8aec5609
Serge ARADJ <saradj@expertime.com>
2019-10-28 12:03:45 +0100

    Fixed URL rewriting for appmobile

--- resources/js/popup.js
+++ resources/js/popup.js
@@ -22,7 +22,7 @@
         /**************
          * App version
          **************/
-        $scope.appVers = "6.4.93";
+        $scope.appVers = "6.4.94";
 
         /*****************
          * View variables
